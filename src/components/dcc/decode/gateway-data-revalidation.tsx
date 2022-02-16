a8564c7b03582de219048dcaad0110a03225f3d2
andypandy47 <apgk95@hotmail.co.uk>
2022-02-16 10:06:53 +0000

    Update key fetching

--- src/components/dcc/decode/gateway-data-revalidation.tsx
+++ src/components/dcc/decode/gateway-data-revalidation.tsx
@@ -3,6 +3,7 @@ import { Flex, IconButton, Text } from '@chakra-ui/react';
 import dayjs from 'dayjs';
 import { useGBNationalBackend } from 'hooks/useGBNationalBackend';
 import * as React from 'react';
+import { INationalBackendPublicKey } from 'services/crypto-interfaces';
 
 const GatewayDataRevalidation: React.FC = () => {
   const { nationalBackendData, setNationalBackendData } =
@@ -14,24 +15,21 @@ const GatewayDataRevalidation: React.FC = () => {
     setIsLoading(true);
 
     try {
-      //const prodDataResponse = await fetch(process.env.NEXT_PUBLIC_NB_PROD ?? '');
+      // const prodDataResponse = await fetch(
+      //   process.env.NEXT_PUBLIC_NB_PROD ?? ''
+      // );
 
       const accDataResponse = await fetch(process.env.NEXT_PUBLIC_NB_ACC ?? '');
-      console.log(process.env.NEXT_PUBLIC_NB_ACC);
-      console.log(accDataResponse);
 
-      if (!accDataResponse.ok) {
-        throw new Error(accDataResponse.statusText);
-      }
-
-      //const prodKeys = await prodDataResponse.json();
-      const accKeys = await accDataResponse.json();
+      //const prodKeys = prodDataResponse.ok ? await prodDataResponse.json() : [];
+      const prodKeys: INationalBackendPublicKey[] = [];
+      const accKeys = accDataResponse.ok ? await accDataResponse.json() : [];
 
       const newDate = dayjs().format('YYYY-MM-DD HH:mm:ss');
 
       setNationalBackendData({
-        PROD: [],
-        ACC: accKeys,
+        PROD: prodKeys.length > 0 ? prodKeys : nationalBackendData.PROD,
+        ACC: accKeys.length > 0 ? accKeys : nationalBackendData.ACC,
         lastFetched: newDate
       });
 
